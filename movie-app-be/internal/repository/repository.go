9a7ed3e58d68c84c39f8641d65608632cf8f6d8f
Tat Hoang <bktathoang@gmail.com>
2023-05-29 16:37:00 +0700

    update

--- movie-app-be/internal/repository/repository.go
+++ movie-app-be/internal/repository/repository.go
@@ -10,4 +10,12 @@ type DatabaseRepo interface {
 	AllMovies() ([]*models.Movie, error)
 	GetUserByEmail(email string) (*models.User, error)
 	GetUserByID(id int) (*models.User, error)
+
+	OneMovieForEdit(id int) (*models.Movie, []*models.Genre, error)
+	OneMovie(id int) (*models.Movie, error)
+	AllGenres() ([]*models.Genre, error)
+	InsertMovie(movie models.Movie) (int, error)
+	UpdateMovieGenres(id int, genreIDs []int) error
+	UpdateMovie(movie models.Movie) error
+	DeleteMovie(id int) error
 }
